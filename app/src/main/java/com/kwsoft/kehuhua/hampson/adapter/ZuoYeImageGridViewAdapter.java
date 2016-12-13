8377a2235f373954144db7a0b80ea399400cc64e
rainsonking <rainsonking@163.com>
2016-12-13 09:45:36 +0800

    修复大图加载和pdf打开

--- app/src/main/java/com/kwsoft/kehuhua/hampson/adapter/ZuoYeImageGridViewAdapter.java
+++ app/src/main/java/com/kwsoft/kehuhua/hampson/adapter/ZuoYeImageGridViewAdapter.java
@@ -3,20 +3,39 @@ package com.kwsoft.kehuhua.hampson.adapter;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.MimeTypeMap;
 import android.widget.BaseAdapter;
+import android.widget.Toast;
 
+import com.facebook.binaryresource.BinaryResource;
+import com.facebook.binaryresource.FileBinaryResource;
+import com.facebook.cache.common.CacheKey;
 import com.facebook.drawee.drawable.ScalingUtils;
 import com.facebook.drawee.generic.GenericDraweeHierarchyBuilder;
 import com.facebook.drawee.view.SimpleDraweeView;
+import com.facebook.imagepipeline.cache.DefaultCacheKeyFactory;
+import com.facebook.imagepipeline.core.ImagePipelineFactory;
+import com.facebook.imagepipeline.request.ImageRequest;
 import com.kwsoft.kehuhua.adcustom.R;
 import com.kwsoft.kehuhua.hampson.activity.ZoomImageActivity;
 import com.kwsoft.kehuhua.utils.NoDoubleClickListener;
 import com.kwsoft.kehuhua.view.ImageLoadingDrawable;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.List;
+import java.util.Locale;
+
+import static com.kwsoft.kehuhua.config.Constant.sdPath;
+import static com.kwsoft.kehuhua.config.Constant.sdPathProject;
 
 
 /**
@@ -26,12 +45,12 @@ import java.util.List;
 public class ZuoYeImageGridViewAdapter extends BaseAdapter {
     private Context context;
     private List<String> datas;
-
+    List<String> fileNames;
 
     public ZuoYeImageGridViewAdapter(Context context, List<String> datas, List<String> fileNames) {
         this.context = context;
         this.datas = datas;
-        List<String> fileNames1 = fileNames;
+        this.fileNames = fileNames;
 
 
     }
@@ -75,11 +94,137 @@ public class ZuoYeImageGridViewAdapter extends BaseAdapter {
         my_image_view.setOnClickListener(new NoDoubleClickListener() {//禁止急速连续点击
             @Override
             public void onNoDoubleClick(View view) {
-                Intent intent = new Intent(context, ZoomImageActivity.class);
-                intent.putExtra("url", url);
-                context.startActivity(intent);
+                File file = getFileFromDiskCache(url);
+                if (file!=null) {
+                    String lowerCase=fileNames.get(i).toLowerCase();
+                    if (lowerCase.endsWith(".jpg")||lowerCase.endsWith(".png")||lowerCase.endsWith(".gif")) {
+                        Intent intent = new Intent(context, ZoomImageActivity.class);
+                        intent.putExtra("url", url);
+                        context.startActivity(intent);
+                    }else{
+                        File file1 = changeName(file, fileNames.get(i));
+                        openFile(file1.getPath());
+
+                    }
+
+                }
+
+
             }
         });
         return view;
+    }
+
+    private File changeName(File beforefile, String newName) {
+        Log.e(TAG, "changeName: 开始修改");
+        //这是你的源文件，本身是存在的
+
+
+//这是你要保存之后的文件，是自定义的，本身不存在
+        File afterFile = new File(sdPath + sdPathProject + newName);
+        Log.e(TAG, "changeName: 监测点0 " + sdPath + newName);
+//定义文件输入流，用来读取beforefile文件
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream(beforefile);
+            Log.e(TAG, "changeName: 监测点1");
+
+            File dirFile = new File(sdPath + newName);
+            if (!dirFile.exists()) {
+                dirFile.mkdirs(); // 第一段
+            }
+
+            if (!afterFile.getParentFile().exists()) {
+                // 分两次mkdirs，是为了避免目录层级过高导致目录创建失败的情况
+                afterFile.getParentFile().mkdirs();
+            }
+            if (!afterFile.exists()) {
+                try {
+                    afterFile.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Log.e(TAG, "changeName: 创建新文件失败");
+                }
+            }
+            //定义文件输出流，用来把信息写入afterfile文件中
+            FileOutputStream fos = new FileOutputStream(afterFile);
+            Log.e(TAG, "changeName: 监测点2");
+            //文件缓存区
+            byte[] b = new byte[1024];
+            Log.e(TAG, "changeName: 监测点3");
+            //将文件流信息读取文件缓存区，如果读取结果不为-1就代表文件没有读取完毕，反之已经读取完毕
+            try {
+                while (fis.read(b) != -1) {
+                    Log.e(TAG, "changeName: 监测点4");
+                    //将缓存区中的内容写到afterfile文件中
+                    fos.write(b);
+                    Log.e(TAG, "changeName: 监测点5");
+                    fos.flush();
+                    Log.e(TAG, "changeName: 监测点6");
+
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.e(TAG, "changeName: 写入失败");
+            }
+
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            Log.e(TAG, "changeName: 修改失败");
+        }
+        return afterFile;
+    }
+
+
+    public static File getFileFromDiskCache(String url) {
+
+        File localFile = null;
+        if (!TextUtils.isEmpty(url)) {
+            CacheKey cacheKey = DefaultCacheKeyFactory.getInstance()
+                    .getEncodedCacheKey(ImageRequest.fromUri(url), null);
+            if (ImagePipelineFactory.getInstance().getMainFileCache().hasKey(cacheKey)) {
+                BinaryResource resource = ImagePipelineFactory.getInstance().getMainFileCache().getResource(cacheKey);
+                localFile = ((FileBinaryResource) resource).getFile();
+            } else if (ImagePipelineFactory.getInstance().getSmallImageFileCache().hasKey(cacheKey)) {
+                BinaryResource resource = ImagePipelineFactory.getInstance().getSmallImageFileCache().getResource(cacheKey);
+                localFile = ((FileBinaryResource) resource).getFile();
+            }
+        }
+        return localFile;
+
+    }
+
+    private static final String TAG = "ZuoYeGridViewAdapter";
+
+    /**
+     */
+    private void openFile(final String filePath) {
+        Log.e(TAG, "openFile: filePath " + filePath);
+        String ext = (filePath.substring(filePath.lastIndexOf('.')).toLowerCase(Locale.US));
+//        String ext =ext1.replace(".","");//适配安卓7.0，截取的时候7.0会包含文件后缀前的那个“点”
+        Log.e(TAG, "openFile: ext "+ext);
+        try {
+            MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+            Log.e(TAG, "changeName: 监测点7");
+            String temp = ext.substring(1);
+            Log.e(TAG, "changeName: 监测点8");
+            String mime = mimeTypeMap.getMimeTypeFromExtension(temp);
+            Log.e(TAG, "changeName: 监测点9");
+            Intent intent = new Intent();
+            Log.e(TAG, "changeName: 监测点10");
+            intent.setAction(android.content.Intent.ACTION_VIEW);
+            Log.e(TAG, "changeName: 监测点11");
+            File file = new File(filePath);
+            Log.e(TAG, "changeName: 监测点12");
+            intent.setDataAndType(Uri.fromFile(file), mime);
+            Log.e(TAG, "changeName: 监测点13");
+            context.startActivity(intent);//需要更改为fileProvider
+            Log.e(TAG, "changeName: 监测点14");
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(context, "无法打开后缀名为." + ext + "的文件！",
+                    Toast.LENGTH_LONG).show();
+        }
     }
 }
