58d746481eadac445b1b0ca3131c9e29b61c4ced
rainsonking <rainsonking@163.com>
2017-01-09 14:41:54 +0800

    调整UI

---
+++ zxingdemo/build/generated/source/apt/release/cn/bingoogolapple/qrcode/zxingdemo/BR.java
@@ -0,0 +1,5 @@
+package cn.bingoogolapple.qrcode.zxingdemo;
+
+public class BR {
+        public static int _all = 0;
+}
---
+++ zxingdemo/build/generated/source/dataBinding/release/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="529bbc30-43df-4cc5-9216-25e1eded90b0", modulePackage="cn.bingoogolapple.qrcode.zxingdemo", sdkRoot="E:\\D\\A\\sdk",layoutInfoDir="E:\\D\\kehuhuaPrimaVersion\\zxingdemo\\build\\intermediates\\data-binding-info\\release",exportClassListTo="E:\\D\\kehuhuaPrimaVersion\\zxingdemo\\build\\intermediates\\data-binding-info\\release\\_generated.txt",isLibrary=true,minSdk=14,enableDebugLogs=false,printEncodedError=true)
+public class DataBindingInfo {}
